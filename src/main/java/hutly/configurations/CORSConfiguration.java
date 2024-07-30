627b465113d75bccabfbb8ec7db9e6ffe6a22d9a
Larisa Becket <lbikteeva@gmail.com>
2024-07-30 10:33:16 +0300

    Fix CORS configuration: allow all origins by updating .allowedOrigins("*")

--- src/main/java/hutly/configurations/CORSConfiguration.java
+++ src/main/java/hutly/configurations/CORSConfiguration.java
@@ -13,7 +13,7 @@ public class CORSConfiguration implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://hutly-landing-page.onrender.com")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
